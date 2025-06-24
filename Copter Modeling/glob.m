c7f4ac2eaebae35f9f42d8d74c979dc6e8b077dd
vini_1080p.mkv <rodriguesviniciusfreitas@gmail.com>
2025-06-24 18:49:51 -0300

    Begin parallel PID discretization correction

--- Copter Modeling/glob.m
+++ Copter Modeling/glob.m
@@ -25,3 +25,82 @@ jr = jp+jm/r;  % [kg.m2]
 
 % Variáveis do Controle
 dt = 0.01; % [s]
+
+controller1_freq_hz = 400;
+controller2_freq_hz = 100;
+controller3_freq_hz = 50;
+controller4_freq_hz = 20;
+
+% Parametros dos controladores
+Kp_roll_rate = tf(0.135);
+Ki_roll_rate = tf(0.135, [1 0]);
+Kd_roll_rate = tf([0.0036 0], 1);
+
+Kp_pitch_rate = tf(0.135);
+Ki_pitch_rate = tf(0.135, [1 0]);
+Kd_pitch_rate = tf(0.0036, 1);
+
+Kp_yaw_rate = tf(0.135);
+Ki_yaw_rate = tf(0.135, [1 0]);
+Kd_yaw_rate = tf(0.0, 1);
+
+Kp_roll = tf(4.5);
+
+Kp_pitch = tf(4.5);
+
+Kp_yaw = tf(4.5);
+
+Kp_x_rate = tf(1);
+Ki_x_rate = tf(0.5, [1 0]);
+Kd_x_rate = tf(0.0, 1);
+
+Kp_y_rate = tf(1);
+Ki_y_rate = tf(0.5, [1 0]);
+Kd_y_rate = tf(0.0, 1);
+
+Kp_z_rate = tf(5);
+Ki_z_rate = tf(2, [1 0]);
+Kd_z_rate = tf(0.0, 1);
+
+Kp_x = tf(1);
+
+Kp_y = tf(1);
+
+Kp_z = tf(1);
+
+% Parâmetros dos controladores discretizados
+Kp_roll_rate_z = c2d(Kp_roll_rate, 1/controller1_freq_hz, 'tustin').Numerator{1};
+Ki_roll_rate_z = c2d(Ki_roll_rate, 1/controller1_freq_hz, 'tustin').Numerator;
+Kd_roll_rate_z = c2d(Kd_roll_rate, 1/controller1_freq_hz, 'tustin');
+
+Kp_pitch_rate_z = c2d(Kp_pitch_rate, 1/controller1_freq_hz, 'tustin').Numerator{1};
+Ki_pitch_rate_z = c2d(Ki_pitch_rate, 1/controller1_freq_hz, 'tustin');
+Kd_pitch_rate_z = c2d(Kd_pitch_rate, 1/controller1_freq_hz, 'tustin');
+
+Kp_yaw_rate_z = c2d(Kp_yaw_rate, 1/controller1_freq_hz, 'tustin').Numerator{1};
+Ki_yaw_rate_z = c2d(Ki_yaw_rate, 1/controller1_freq_hz, 'tustin');
+Kd_yaw_rate_z = c2d(Kd_yaw_rate, 1/controller1_freq_hz, 'tustin');
+
+Kp_roll_z = c2d(Kp_roll, 1/controller2_freq_hz, 'tustin').Numerator{1};
+
+Kp_pitch_z = c2d(Kp_pitch, 1/controller2_freq_hz, 'tustin').Numerator{1};
+
+Kp_yaw_z = c2d(Kp_pitch, 1/controller2_freq_hz, 'tustin').Numerator{1};
+
+Kp_x_rate_z = c2d(Kp_x_rate, 1/controller3_freq_hz, 'tustin').Numerator{1};
+Ki_x_rate_z = c2d(Ki_x_rate, 1/controller3_freq_hz, 'tustin');
+Kd_x_rate_z = c2d(Kd_x_rate, 1/controller3_freq_hz, 'tustin');
+
+Kp_y_rate_z = c2d(Kp_y_rate, 1/controller3_freq_hz, 'tustin').Numerator{1};
+Ki_y_rate_z = c2d(Ki_y_rate, 1/controller3_freq_hz, 'tustin');
+Kd_y_rate_z = c2d(Kd_y_rate, 1/controller3_freq_hz, 'tustin');
+
+Kp_z_rate_z = c2d(Kp_z_rate, 1/controller3_freq_hz, 'tustin').Numerator{1};
+Ki_z_rate_z = c2d(Ki_z_rate, 1/controller3_freq_hz, 'tustin');
+Kd_z_rate_z = c2d(Kd_z_rate, 1/controller3_freq_hz, 'tustin');
+
+Kp_x_z = c2d(Kp_x, 1/controller4_freq_hz, 'tustin').Numerator{1};
+
+Kp_y_z = c2d(Kp_y, 1/controller4_freq_hz, 'tustin').Numerator{1};
+
+Kp_z_z = c2d(Kp_z, 1/controller4_freq_hz, 'tustin').Numerator{1};
